9deb39cfdc9d2ff2b9155725e8198d2fb0192777
Pavan <pa1.omanwar@outlook.com>
2018-06-18 14:48:16 -0700

    Update csvToHive.sh

--- csvToHive.sh
+++ csvToHive.sh
@@ -1,45 +1,27 @@
 #!/bin/sh
 
-n=1
-#continue for 4 tries 
 
-while [ $n -le 4 ]
+hadoop fs -put /home/cloudera/Documents/LatestCSV/user_upload_dump* workshop/hive/CSVfiles/LatestCSV
 
-do 
+mv /home/cloudera/Documents/LatestCSV/user_upload_dump* /home/cloudera/Documents/CSVDump
 
-	echo Loading CSVs to Hive...........
-
-	hadoop fs -mkdir workshop/
-	hadoop fs -mkdir workshop/hive/
-	hadoop fs -mkdir workshop/hive/CSVfiles/
-	hadoop fs -put csvToUpload.csv workshop/hive/CSVfiles/
-
-	hive -e "DROP TABLE IF EXISTS hive_practical_exercise_1.csv;"
-
-	hive -e "CREATE TABLE hive_practical_exercise_1.csv ( user_id int, file_name String, timestamp int) row format delimited fields terminated by ',' stored as textfile tblproperties ('skip.header.line.count'='1');"
-
-	hive -e "LOAD DATA INPATH 'workshop/hive/CSVfiles/csvToUpload.csv' OVERWRITE INTO TABLE hive_practical_exercise_1.csv;"
-
-	hive -e "SELECT * FROM hive_practical_exercise_1.csv;"
+hadoop fs -cp workshop/hive/CSVfiles/LatestCSV/user_upload_dump* workshop/hive/CSVfiles/CSVDump
 
 
-	if [ $? -eq 0 ]
-	then
-	  echo Successful!
-	  exit 0
-	  break
-        else 
-          echo Process failed, Trying again......
+for (( c=1; c<=5; c++ )) 
+do
+
+
+	hive -e "LOAD DATA INPATH 'workshop/hive/CSVfiles/LatestCSV/*' OVERWRITE INTO TABLE hive_practical_exercise_1.csv;"
+
+	if [ $? -eq 0 ]; then
+	  echo successfully imported data to csv table...
+          break
+	else
+	  echo failed to import data to csv table...
 	fi
-
-	n=$(( n+1 ))	 # increments $n
-
 
 done
 
-if [$n == 5]
-then
-   echo Job Failed
-   exit 1
-fi
 
+hive -e "SELECT * FROM hive_practical_exercise_1.csv;"
