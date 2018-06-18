d3eb9fb879243e499584aaac803af85cf8a11518
Pavan <pa1.omanwar@outlook.com>
2018-06-18 14:50:21 -0700

    Update ReportingTable2.sh

--- ReportingTable2.sh
+++ ReportingTable2.sh
@@ -1,14 +1,21 @@
 #!/bin/sh
 
-NOW=$(date +"%s")
-
-hive -e "DROP TABLE IF EXISTS hive_practical_exercise_1.user_total;"
 
 
-hive -e "create TABLE hive_practical_exercise_1.user_total(time_ran int, total_users int , users_added int);"
+for (( c=1; c<=5; c++ )) 
+do
 
-hive -e "Insert into hive_practical_exercise_1.user_total select current_timestamp() , a.cnt , CASE when b.cnt2 is not null then a.cnt-b.cnt2 else null end from (select count(DISTINCT id) cnt from hive_practical_exercise_1.user ) as a, (select max(total_users) cnt2 from hive_practical_exercise_1.user_total ) as b ;"
+	hive -e "Insert into hive_practical_exercise_1.user_total select current_timestamp() , a.cnt , CASE when b.cnt2 is not null then a.cnt-b.cnt2 else null end from (select count(DISTINCT id) cnt from hive_practical_exercise_1.user ) as a, (select max(total_users) cnt2 from hive_practical_exercise_1.user_total ) as b ;"
 
+	if [ $? -eq 0 ]; then
+	  echo success
+          break
+	else
+	  echo failed to insert
+	fi
+
+
+done
 hive -e "SELECT * FROM hive_practical_exercise_1.user_total;"
 
 
