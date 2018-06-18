a951b366437c3c3a88c4fb60a687e6d3d3d2927b
Pavan <pa1.omanwar@outlook.com>
2018-06-18 14:46:53 -0700

    updated

--- MysqlToHive.sh
+++ MysqlToHive.sh
@@ -2,15 +2,37 @@
 
 echo "Importing Data to Hive....."
 
-sqoop import --connect jdbc:mysql://localhost/practical_exercise_1 --username root --password-file /user/cloudera/root_pwd.txt --table user -m 4 --hive-import --hive-overwrite --hive-database hive_practical_exercise_1 --hive-table user
+for (( c=1; c<=5; c++ )) 
+do
+
+	sqoop import --connect jdbc:mysql://localhost/practical_exercise_1 --username root --password-file /user/cloudera/root_pwd.txt --table user -m 4 --hive-import --hive-overwrite --hive-database hive_practical_exercise_1 --hive-table user
+
+	if [ $? -eq 0 ]; then
+	  echo successfully imported user table...
+          break
+	else
+	  echo failed to import user table...
+	fi
+
+done
 
 
-sqoop job --meta-connect jdbc:hsqldb:hsql://localhost:16000/sqoop  -- import --connect jdbc:mysql://localhost/practical_exercise_1 --username root --password-file /user/cloudera/root_pwd.txt --table activitylog -m 4 --hive-import --hive-database hive_practical_exercise_1 --hive-table activitylog --incremental append --check-column id --last-value 0
+for (( b=1; b<=5; b++ )) 
+do
+
+	sqoop job --meta-connect jdbc:hsqldb:hsql://localhost:16000/sqoop  -- import --connect jdbc:mysql://localhost/practical_exercise_1 --username root --password-file /user/cloudera/root_pwd.txt --table activitylog -m 4 --hive-import --hive-database hive_practical_exercise_1 --hive-table activitylog --incremental append --check-column id --last-value 0
 
 
-sqoop job --meta-connect jdbc:hsqldb:hsql://localhost:16000/sqoop --exec hive_practical_exercise_1.activitylog
+	sqoop job --meta-connect jdbc:hsqldb:hsql://localhost:16000/sqoop --exec hive_practical_exercise_1.activitylog
 
-echo "....Data Imported to Hive"
+	if [ $? -eq 0 ]; then
+	  echo successfully imported activitylog table...
+	  break
+	else
+	  echo failed to import activitylog table...
+	fi
+
+done
 
 hive -e "SELECT * FROM hive_practical_exercise_1.user;"
 
